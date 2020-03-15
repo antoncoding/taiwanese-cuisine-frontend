b6a705a4b558971f868bb1d1a82a001ba4c264d3
Anton Cheng <anton@coolbitx.com>
2020-03-16 00:49:01 +0800

    Updates

---
+++ precache-manifest.3dfd7e37d6b927d9dddd363e75f51868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc5193f3273d085d1d16ba4c3c93cf3",
+    "url": "/taiwanese-cuisine-frontend/index.html"
+  },
+  {
+    "revision": "f17b302d1c0704a111ce",
+    "url": "/taiwanese-cuisine-frontend/static/css/2.596724bf.chunk.css"
+  },
+  {
+    "revision": "cdc2449b7441869dbd7b",
+    "url": "/taiwanese-cuisine-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f17b302d1c0704a111ce",
+    "url": "/taiwanese-cuisine-frontend/static/js/2.b68da609.chunk.js"
+  },
+  {
+    "revision": "0a457af33bb9bae46083520515d84114",
+    "url": "/taiwanese-cuisine-frontend/static/js/2.b68da609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc2449b7441869dbd7b",
+    "url": "/taiwanese-cuisine-frontend/static/js/main.7ff20320.chunk.js"
+  },
+  {
+    "revision": "d6de77cc24cc2872c713",
+    "url": "/taiwanese-cuisine-frontend/static/js/runtime-main.66e61c18.js"
+  }
+]);
