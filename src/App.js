42225d3fc732684ad206b18aed0a132288661c06
Anton Cheng <anton@coolbitx.com>
2020-03-28 21:02:51 +0800

    Test log keys

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 
 function App() {
+
+  const key = process.env.REACT_APP_TEST_KEY
+  console.log(`got env key ${key}`)
+
   return (
     <Router>
         <Switch>
