f74692cc11947c9e800a3fbeb8778b45ce64d1bd
antoncoding <antonassocareer@gmail.com>
2020-03-28 13:03:43 +0000

    Deploying to gh-pages from  - f5367417db501696cff34bd559272ce0b7d64389 🚀

---
+++ precache-manifest.b51cb5c5235b80e8fda7f752e493fc51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250462ece7fe00cfd2bc04ff748b58e9",
+    "url": "/taiwanese-cuisine-frontend/index.html"
+  },
+  {
+    "revision": "ec95f6f275b10d005833",
+    "url": "/taiwanese-cuisine-frontend/static/css/2.596724bf.chunk.css"
+  },
+  {
+    "revision": "9dc56c27eba27445c559",
+    "url": "/taiwanese-cuisine-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ec95f6f275b10d005833",
+    "url": "/taiwanese-cuisine-frontend/static/js/2.8cd7d55f.chunk.js"
+  },
+  {
+    "revision": "0a457af33bb9bae46083520515d84114",
+    "url": "/taiwanese-cuisine-frontend/static/js/2.8cd7d55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc56c27eba27445c559",
+    "url": "/taiwanese-cuisine-frontend/static/js/main.c49c7818.chunk.js"
+  },
+  {
+    "revision": "d6de77cc24cc2872c713",
+    "url": "/taiwanese-cuisine-frontend/static/js/runtime-main.66e61c18.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taiwanese-cuisine-frontend/precache-manifest.ae364354e73969f13515e9d36dcf6c72.js"
+  "/taiwanese-cuisine-frontend/precache-manifest.b51cb5c5235b80e8fda7f752e493fc51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taiwanese-cuisine-frontend/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
