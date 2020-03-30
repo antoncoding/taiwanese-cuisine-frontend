64726ccc6a6015e582571b511d2b26106fc39be2
Anton Cheng <anton@coolbitx.com>
2020-03-30 15:15:35 +0800

    Try dynamo db

--- src/Components/Compare/utils.js
+++ src/Components/Compare/utils.js
@@ -2,7 +2,7 @@ const PREFIX = 'https://cors-anywhere-anton.herokuapp.com/'
 // const PREFIX = ''
 
 export const vote = async (winner, loser) => {
-    fetch(PREFIX + `http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/votef?loser=${loser}&winner=${winner}`).then(res => {
+    fetch(PREFIX + `http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/votec?loser=${loser}&winner=${winner}`).then(res => {
       console.log(res)
     }).catch(error => {
       console.error(error)
@@ -14,7 +14,7 @@ export const vote = async (winner, loser) => {
  * @return {Promise<{Name:string, Image:string, Played: number, Score: number}[]>}
  */
 export const getList = async () => {
-  const response = await fetch(PREFIX + 'http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/cuisinef/');
+  const response = await fetch(PREFIX + 'http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/cuisinec/');
   const list = await response.json()
   return list
 }
