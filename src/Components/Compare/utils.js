916eac00b0e2c76d059d46cffd133f4962c64cc2
Anton Cheng <anton@coolbitx.com>
2020-03-30 18:01:13 +0800

    Test prefix

--- src/Components/Compare/utils.js
+++ src/Components/Compare/utils.js
@@ -1,8 +1,6 @@
-const PREFIX = 'https://cors-anywhere-anton.herokuapp.com/'
-// const PREFIX = ''
 
 export const vote = async (winner, loser) => {
-    fetch(PREFIX + `http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/votec?loser=${loser}&winner=${winner}`).then(res => {
+    fetch(`${process.env.REACT_APP_CORS_PREFIX || ''}http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/votec?loser=${loser}&winner=${winner}`).then(res => {
       console.log(res)
     }).catch(error => {
       console.error(error)
@@ -14,7 +12,7 @@ export const vote = async (winner, loser) => {
  * @return {Promise<{Name:string, Image:string, Played: number, Score: number}[]>}
  */
 export const getList = async () => {
-  const response = await fetch(PREFIX + 'http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/cuisinec/');
+  const response = await fetch(`${process.env.REACT_APP_CORS_PREFIX || ''}http://taiwanese-cuisine-backend-dev.ap-northeast-2.elasticbeanstalk.com/cuisinec/`);
   const list = await response.json()
   return list
 }
